a8a3c2da9a03b4b3a2800e3a9ad7264a96c74433
fgsfds <pvt.fgsfds@gmail.com>
2018-11-23 00:57:09 +0300

    switch: update version

--- nx/version.h
+++ nx/version.h
@@ -1,8 +1,8 @@
 /* Define to the library version */
-#define ALSOFT_VERSION "1.19.0"
+#define ALSOFT_VERSION "1.19.1"
 
 /* Define the branch being built */
 #define ALSOFT_GIT_BRANCH "nx"
 
 /* Define the hash of the head commit */
-#define ALSOFT_GIT_COMMIT_HASH "36a6b9d4"
+#define ALSOFT_GIT_COMMIT_HASH "ac6a9aeb"
